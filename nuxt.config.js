07364a0a60577f00e6c8c88efca4959ef0623bf0
Diogo Condêço <diogobcondeco@gmail.com>
2020-08-28 21:37:33 +0100

    🔧 Added a title to the header

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,7 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
+    titleTemplate: 'Diogo Condêço - Front End Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
