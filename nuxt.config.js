110f57f5650bbec90243adf1615caa218a2ffd05
Diogo Condêço <diogobcondeco@gmail.com>
2020-08-28 22:53:53 +0100

    🔧 Updated meta description content

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: "I'm a Front End Developer that loves to build things." }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
